527fab5da13847ec3cff50d6b4e2dd09804b481f
ocharzuke <ocharzuke@gmail.com>
2025-06-28 15:15:34 +0800

    first commit with basic storefront done in Reac t

---
+++ src/ProductCard.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+
+const ProductCard = (props) => {
+  return (
+    <div className="card shadow-sm">
+      <img
+        src={props.imageUrl}
+        className="card-img-top"
+        alt={props.productName}
+      />
+      <div className="card-body">
+        <h5 className="card-title">{props.productName}</h5>
+        <p className="card-text">{props.productDescription}</p>
+        <div className="d-flex justify-content-between align-items-center">
+          <small className="text-body-secondary">${props.price}</small>
+          <div className="btn-group">
+            <button type="button" className="btn btn-sm btn-outline-secondary">
+              Details
+            </button>
+            <button type="button" className="btn btn-sm btn-outline-danger">
+              Add To Cart
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ProductCard;
